2d54f84681a9662adcc7d54b28310d18f31a8a01
Andrey Dyldin <and@cesbo.com>
2012-12-09 14:38:45 +0600

    frontend name

--- dvbls.c
+++ dvbls.c
@@ -13,11 +13,14 @@
 #include <string.h>
 #include <fcntl.h>
 #include <dirent.h>
+#include <linux/dvb/version.h>
+#include <linux/dvb/frontend.h>
 #include <linux/dvb/net.h>
 #include <errno.h>
 
 static int adapter = 0;
 static int device = 0;
+static char dev_name[512];
 
 static void iterate_dir(const char *dir, const char *filter, void (*callback)(const char *))
 {
@@ -67,24 +70,21 @@ static int get_last_int(const char *str)
     return atoi(&str[i_pos]);
 }
 
-static void check_net_device(const char *item)
+static void check_device_net(void)
 {
-    device = get_last_int(&item[(sizeof("/dev/dvb/adapter") - 1) + (sizeof("/net") - 1)]);
+    sprintf(dev_name, "/dev/dvb/adapter%d/net%d", adapter, device);
 
-    int ret = 0;
-
-    int fd = open(item, O_RDWR | O_NONBLOCK);
+    int fd = open(dev_name, O_RDWR | O_NONBLOCK);
     if(!fd)
     {
-        printf("ERROR: open() failed %s [%s]\n", item, strerror(errno));
+        printf("ERROR: open() failed %s [%s]\n", dev_name, strerror(errno));
         return;
     }
 
-    struct dvb_net_if net;
-    net.pid = 0;
+    struct dvb_net_if net = { 0, 0 ,0 };
     if(ioctl(fd, NET_ADD_IF, &net) < 0)
     {
-        printf("ERROR: NET_ADD_IF failed %s [%s]\n", item, strerror(errno));
+        printf("ERROR: NET_ADD_IF failed %s [%s]\n", dev_name, strerror(errno));
         close(fd);
         return;
     }
@@ -95,26 +95,59 @@ static void check_net_device(const char *item)
 
     int sock = socket(PF_INET, SOCK_DGRAM, 0);
     if(ioctl(sock, SIOCGIFHWADDR, &ifr) < 0)
-        printf("ERROR: SIOCGIFHWADDR failed %s [%s]\n", item, strerror(errno));
+        printf("ERROR: SIOCGIFHWADDR failed %s [%s]\n", dev_name, strerror(errno));
     else
     {
         const unsigned char *mac = ifr.ifr_hwaddr.sa_data;
-        printf("adapter:%d device:%d mac:%02X:%02X:%02X:%02X:%02X:%02X\n"
-               , adapter, device
+        printf("    mac:%02X:%02X:%02X:%02X:%02X:%02X\n"
                , mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
     }
     close(sock);
 
     if(ioctl(fd, NET_REMOVE_IF, net.if_num) < 0)
-        printf("ERROR: NET_REMOVE_IF failed %s [%s]\n", item, strerror(errno));
+        printf("ERROR: NET_REMOVE_IF failed %s [%s]\n", dev_name, strerror(errno));
 
     close(fd);
+}
+
+static void check_device_frontend(void)
+{
+    sprintf(dev_name, "/dev/dvb/adapter%d/frontend%d", adapter, device);
+
+    int fd = open(dev_name, O_RDWR | O_NONBLOCK);
+    if(!fd)
+    {
+        printf("ERROR: open() failed %s [%s]\n", dev_name, strerror(errno));
+        return;
+    }
+
+    struct dvb_frontend_info feinfo;
+    if(ioctl(fd, FE_GET_INFO, &feinfo) < 0)
+    {
+        printf("ERROR: FE_GET_INFO failed %s [%s]\n", dev_name, strerror(errno));
+        close(fd);
+        return;
+    }
+
+    printf("    frontend:%s\n", feinfo.name);
+
+    close(fd);
+}
+
+static void check_device(const char *item)
+{
+    device = get_last_int(&item[(sizeof("/dev/dvb/adapter") - 1) + (sizeof("/net") - 1)]);
+
+    printf("adapter:%d device:%d\n", adapter, device);
+    check_device_frontend();
+    check_device_net();
+    printf("\n");
 }
 
 static void check_adapter(const char *item)
 {
     adapter = get_last_int(&item[sizeof("/dev/dvb/adapter") - 1]);
-    iterate_dir(item, "net", check_net_device);
+    iterate_dir(item, "net", check_device);
 }
 
 int main(int argc, char const *argv[])
